cecaf9f7b9d005347d122d800a082cdf9b295e7a
Sebastian Skałacki <skalee@gmail.com>
2018-01-10 03:40:29 +0100

    Switch to modern RSpec syntax

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -15,4 +15,8 @@ ThreadVarAccessor::Logger.initialize_logger(
 RSpec.configure do |config|
   # Enable flags like --only-failures and --next-failure
   config.example_status_persistence_file_path = ".rspec_status"
+
+  config.expect_with :rspec do |c|
+    c.syntax = :expect
+  end
 end
