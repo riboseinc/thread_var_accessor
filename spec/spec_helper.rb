fe02bda949cfc8acc102ad16c853d67b261a3cc8
Sebastian Skałacki <skalee@gmail.com>
2018-01-10 03:39:36 +0100

    Update RSpec configuration
    Require support files (if any), and generate .rspec-status file.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,7 +5,14 @@
 require "bundler"
 Bundler.require :default, :development
 
+Dir[File.expand_path "../support/**/*.rb", __FILE__].sort.each { |f| require f }
+
 # initialize logger
 ThreadVarAccessor::Logger.initialize_logger(
   File.expand_path("../debug.log", __FILE__),
 )
+
+RSpec.configure do |config|
+  # Enable flags like --only-failures and --next-failure
+  config.example_status_persistence_file_path = ".rspec_status"
+end
